6f320e54627ad7a952312a8e6f0b8e12b97c2ff3
I. YOON <tingcobell@gmail.com>
2020-09-30 23:21:31 +0900

    예제 도메인 모델 추가. - Member - Team

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,46 @@
+package study.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String name) {
+        this(name, 0);
+    }
+
+    public Member(String name, int age) {
+        this(name, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        this.team = team;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMemberList().add(this);
+    }
+
+    public void changeName(String username) {
+        this.username = username;
+    }
+}
