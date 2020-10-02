7c65e442c687c0e66d88ee3343794ebb6cab297f
I. YOON <tingcobell@gmail.com>
2020-10-02 20:14:45 +0900

    테스트 추가.

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,91 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+import study.querydsl.repository.MemberRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamA);
+        Member member4 = new Member("member4", 40, teamB);
+        Member member5 = new Member("member5", 50, teamB);
+        Member member6 = new Member("member6", 60, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        em.persist(member5);
+        em.persist(member6);
+
+        em.flush();
+        em.clear();
+    }
+
+    @Test
+    void startQuerydsl() {
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void findAll() {
+        // which
+
+        //when
+        QMember m = new QMember("m");
+        List<Member> findList = queryFactory
+                .select(m)
+                .from(m)
+                .fetch();
+
+        //then
+        assertThat(findList.size()).isEqualTo(6);
+
+        for (Member member : findList) {
+            System.out.println("member = " + member);
+        }
+    }
+}
